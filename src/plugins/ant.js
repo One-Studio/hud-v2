168977a91c958ea1d9556b5774d8a45842bc307f
TEDIORELEE <tediorelee@qq.com>
2021-03-18 21:37:46 +0800

    update

--- src/plugins/ant.js
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue'
-// import Button from 'ant-design-vue/lib/button'
-import AntD from 'ant-design-vue'
-import 'ant-design-vue/dist/antd.css'
-
-// Vue.component(Button.name, Button)
-Vue.use(AntD)
-
-Vue.config.productionTip = false
